34da23b899f5f39536089ac200a6642245446b09
altbdoor <lancersupraskyline@gmail.com>
2024-10-09 21:41:46 +0800

    fix build from remaining material

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
       output: {
         manualChunks: {
           codemirror: ["codemirror"],
-          "mui-material": ["@mui/material"],
         },
       },
     },
