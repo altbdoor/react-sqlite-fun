7252f5040085f4530320a0b70e9289b5ed4d5cae
altbdoor <lancersupraskyline@gmail.com>
2024-11-18 22:18:24 +0800

    use react compiler woo

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from "@eslint/js";
-import globals from "globals";
+import reactCompiler from "eslint-plugin-react-compiler";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -16,9 +17,11 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "react-compiler": reactCompiler,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "react-compiler/react-compiler": "error",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const ReactCompilerConfig = {
+  target: "19",
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ["@sqlite.org/sqlite-wasm"],
   },
